fac3b9478d6d43b7cffb10a4fe15a0b2931f7463
Naoto Kumakura <kumakura117@gmail.com>
2023-06-03 15:31:05 +0900

    リクエスト処理を追加

--- app/src/main/java/com/websarva/wings/android/weathercheck/MainActivity.kt
+++ app/src/main/java/com/websarva/wings/android/weathercheck/MainActivity.kt
@@ -2,14 +2,97 @@ package com.websarva.wings.android.weathercheck
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.view.View
+import androidx.lifecycle.lifecycleScope
 import com.websarva.wings.android.weathercheck.databinding.ActivityMainBinding
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
+import org.json.JSONObject
+import java.io.BufferedReader
+import java.io.InputStream
+import java.io.InputStreamReader
+import java.net.HttpURLConnection
+import java.net.SocketTimeoutException
+import java.net.URL
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding : ActivityMainBinding
+
+    companion object {
+        private const val WEATHER_INFO_URL = "https://api.openweathermap.org/data/2.5/weather?"
+
+        private const val API_KEY = BuildConfig.API_KEY
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         val view = binding.root
         setContentView(view)
+    }
+
+    fun getWeatherInformation(view: View) {
+        Log.d("APP_DEBUG","getWeatherInformation開始")
+        val test ="$WEATHER_INFO_URL&q=NIIGATA&appid=$API_KEY"
+        lifecycleScope.launch{
+            val result = requestWeatherInformation(test)
+            val weather = judgeWeather(result)
+        }
+
+        Log.d("APP_DEBUG","getWeatherInformation終了")
+    }
+
+    private suspend fun requestWeatherInformation(url: String) : String{
+        Log.d("APP_DEBUG","リクエスト開始")
+        val returnVal = withContext(Dispatchers.IO) {
+            var result = ""
+            val url = URL(url)
+            val con = url.openConnection() as? HttpURLConnection
+            con?.let {
+                try {
+                    it.connectTimeout = 1000
+                    it.readTimeout = 10000
+                    it.requestMethod = "GET"
+                    it.connect()
+                    val stream = it.inputStream
+                    result = is2String(stream)
+                    stream.close()
+                }catch (ex: SocketTimeoutException) {
+                    Log.w("APP_DEBUG", "通信タイムアウト", ex)
+                }
+
+                it.disconnect()
+
+            }
+            result
+        }
+        Log.d("APP_DEBUG","リクエスト終了")
+        return returnVal
+    }
+
+    private fun is2String(stream: InputStream): String {
+        val sb = StringBuilder()
+        val reader = BufferedReader(InputStreamReader(stream, "UTF-8"))
+        var line = reader.readLine()
+        while(line != null){
+            sb.append(line)
+            line = reader.readLine()
+        }
+        reader.close()
+        return sb.toString()
+    }
+
+    private fun judgeWeather(result: String){
+        Log.d("APP_DEBUG","レスポンス処理開始")
+        val rootJSON = JSONObject(result)
+        val weatherJSONArray = rootJSON.getJSONArray("weather")
+        val weatherJSON = weatherJSONArray.getJSONObject(0)
+
+        val weather = weatherJSON.getString("main")
+        val icon = weatherJSON.getString("icon")
+        Log.d("APP_DEBUG","${weather} : ${icon}")
+        Log.d("APP_DEBUG","レスポンス処理終了")
     }
 }
