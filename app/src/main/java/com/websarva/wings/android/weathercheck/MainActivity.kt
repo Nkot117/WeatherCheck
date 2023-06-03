b767108bee05131678accb74391678cccb03e5eb
Naoto Kumakura <kumakura117@gmail.com>
2023-06-03 16:30:45 +0900

    アノテーションを付与

--- app/src/main/java/com/websarva/wings/android/weathercheck/MainActivity.kt
+++ app/src/main/java/com/websarva/wings/android/weathercheck/MainActivity.kt
@@ -4,6 +4,8 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import android.view.View
+import androidx.annotation.UiThread
+import androidx.annotation.WorkerThread
 import androidx.lifecycle.lifecycleScope
 import com.websarva.wings.android.weathercheck.databinding.ActivityMainBinding
 import kotlinx.coroutines.Dispatchers
@@ -18,11 +20,10 @@ import java.net.SocketTimeoutException
 import java.net.URL
 
 class MainActivity : AppCompatActivity() {
-    private lateinit var binding : ActivityMainBinding
+    private lateinit var binding: ActivityMainBinding
 
     companion object {
         private const val WEATHER_INFO_URL = "https://api.openweathermap.org/data/2.5/weather?"
-
         private const val API_KEY = BuildConfig.API_KEY
     }
 
@@ -33,19 +34,21 @@ class MainActivity : AppCompatActivity() {
         setContentView(view)
     }
 
+    @UiThread
     fun getWeatherInformation(view: View) {
-        Log.d("APP_DEBUG","getWeatherInformation開始")
-        val test ="$WEATHER_INFO_URL&q=NIIGATA&appid=$API_KEY"
-        lifecycleScope.launch{
+        Log.d("APP_DEBUG", "getWeatherInformation開始")
+        val test = "$WEATHER_INFO_URL&q=NIIGATA&appid=$API_KEY"
+        lifecycleScope.launch {
             val result = requestWeatherInformation(test)
             val weather = judgeWeather(result)
         }
 
-        Log.d("APP_DEBUG","getWeatherInformation終了")
+        Log.d("APP_DEBUG", "getWeatherInformation終了")
     }
 
-    private suspend fun requestWeatherInformation(url: String) : String{
-        Log.d("APP_DEBUG","リクエスト開始")
+    @WorkerThread
+    private suspend fun requestWeatherInformation(url: String): String {
+        Log.d("APP_DEBUG", "リクエスト開始")
         val returnVal = withContext(Dispatchers.IO) {
             var result = ""
             val url = URL(url)
@@ -59,7 +62,7 @@ class MainActivity : AppCompatActivity() {
                     val stream = it.inputStream
                     result = is2String(stream)
                     stream.close()
-                }catch (ex: SocketTimeoutException) {
+                } catch (ex: SocketTimeoutException) {
                     Log.w("APP_DEBUG", "通信タイムアウト", ex)
                 }
 
@@ -68,15 +71,16 @@ class MainActivity : AppCompatActivity() {
             }
             result
         }
-        Log.d("APP_DEBUG","リクエスト終了")
+        Log.d("APP_DEBUG", "リクエスト終了")
         return returnVal
     }
 
+    @UiThread
     private fun is2String(stream: InputStream): String {
         val sb = StringBuilder()
         val reader = BufferedReader(InputStreamReader(stream, "UTF-8"))
         var line = reader.readLine()
-        while(line != null){
+        while (line != null) {
             sb.append(line)
             line = reader.readLine()
         }
@@ -84,28 +88,21 @@ class MainActivity : AppCompatActivity() {
         return sb.toString()
     }
 
-    private fun judgeWeather(result: String){
-        Log.d("APP_DEBUG","レスポンス処理開始")
+    @UiThread
+    private fun judgeWeather(result: String) {
+        Log.d("APP_DEBUG", "レスポンス処理開始")
         val rootJSON = JSONObject(result)
         val weatherJSONArray = rootJSON.getJSONArray("weather")
         val weatherJSON = weatherJSONArray.getJSONObject(0)
         val weather = weatherJSON.getString("main")
         val icon = weatherJSON.getString("icon")
-        when(icon.substring(0,2)){
-            "01","02" -> {
-                binding.weatherImage.setImageResource(R.drawable.weather_sun)
-            }
-            "03","04" ->{
-                binding.weatherImage.setImageResource(R.drawable.weather_cloud)
-            }
-            "09","10","11","50" ->{
-                binding.weatherImage.setImageResource(R.drawable.weather_rain)
-            }
-            "13" -> {
-                binding.weatherImage.setImageResource(R.drawable.weather_snow)
-            }
+        when (icon.substring(0, 2)) {
+            "01", "02" -> binding.weatherImage.setImageResource(R.drawable.weather_sun)
+            "03", "04" -> binding.weatherImage.setImageResource(R.drawable.weather_cloud)
+            "09", "10", "11", "50" -> binding.weatherImage.setImageResource(R.drawable.weather_rain)
+            "13" -> binding.weatherImage.setImageResource(R.drawable.weather_snow)
         }
-        Log.d("APP_DEBUG","${weather} : ${icon}")
-        Log.d("APP_DEBUG","レスポンス処理終了")
+        Log.d("APP_DEBUG", "${weather} : ${icon}")
+        Log.d("APP_DEBUG", "レスポンス処理終了")
     }
 }
